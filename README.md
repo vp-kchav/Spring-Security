ef3de522397f0cfff549899c9263853294c918f5
Kimtey CHAV <kimtey.chav@gmail.com>
2023-12-08 10:54:06 -0800

    add working code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Spring-Security
 This is a sample use of spring security
+
+Step: https://www.geeksforgeeks.org/authentication-and-authorization-in-spring-boot-3-0-with-spring-security/
---
+++ src/main/java/com/intuit/playerdb/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.intuit.playerdb.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    // User Creation
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        // InMemoryUserDetailsManager
+        UserDetails admin = User.withUsername("Amiya")
+                .password(encoder.encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user = User.withUsername("Ejaz")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    // Configuring HttpSecurity
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http //.csrf().disable()
+                .authorizeHttpRequests() //(auth -> auth.mvcMatchers("/auth/welcome").permitAll())
+                .mvcMatchers("/auth/welcome").permitAll()
+                .and()
+                .authorizeHttpRequests().mvcMatchers("/auth/user/**").authenticated()
+                .and()
+                .authorizeHttpRequests().mvcMatchers("/auth/admin/**").authenticated()
+                .and().formLogin()
+                .and().build();
+    }
+
+    // Password Encoding
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
